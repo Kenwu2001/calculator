170cca74085c798d5378115193beff6fc81c7e6e
s109000114 <imeukg2001@gmail.com>
2024-04-29 20:52:44 +0800

    Everything Works well, except "multiply 0 problem".

--- Lab_04.js
+++ Lab_04.js
@@ -1,2 +1,168 @@
 // Todo 3 : Write some function to handle the pass value from HTML
 console.log("Fighting!");
+
+function ClickButton0() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == 0) {
+        curNumber = "";
+    }
+    curNumber += '0';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton1() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '1';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton2() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '2';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton3() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '3';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton4() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '4';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton5() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '5';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton6() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '6';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton7() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '7';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton8() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '8';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButton9() {
+    var curNumber = document.getElementsByClassName("display")[0].value;
+    console.log(curNumber);
+    if (curNumber == "0") {
+        curNumber = "";
+    }
+    curNumber += '9';
+    document.getElementsByClassName("display")[0].value = curNumber;
+}
+
+function ClickButtonDot() {
+    document.getElementsByClassName("display")[0].value += '.';
+}
+
+function ClickButtonC() {
+    // console.log("whwh");
+    document.getElementsByClassName("display")[0].value = '0';
+}
+
+function ClickButtonL() {
+    document.getElementsByClassName("display")[0].value += '(';
+}
+
+function ClickButtonR() {
+    document.getElementsByClassName("display")[0].value += ')';
+}
+
+function ClickButtonDivide() {
+    document.getElementsByClassName("display")[0].value += '/';
+}
+
+function ClickButtonMultiply() {
+    document.getElementsByClassName("display")[0].value += '*';
+}
+
+function ClickButtonSub() {
+    document.getElementsByClassName("display")[0].value += '-';
+}
+
+function ClickButtonAdd() {
+    document.getElementsByClassName("display")[0].value += '+';
+}
+
+function ClickButtonEqual() {
+    var nowNum = document.getElementsByClassName("display")[0].value;
+    var ans = 0;
+    try {
+        ans = eval(nowNum);
+        // document.getElementsByClassName("display")[0].value = eval(nowNum);
+    }
+    catch (e) {
+        ans = "Error!!!!";
+        // document.getElementsByClassName("display")[0].value = eval(nowNum);
+    }
+    document.getElementsByClassName("display")[0].value = ans;
+}
+
+function PressKeyboard(event) {
+    var nowNum = document.getElementsByClassName("display")[0].value;
+    console.log("here : ", nowNum);
+    if(event.key == 'Enter') {
+        // var nowNum = document.getElementsByClassName("display")[0].value;
+        var ans = 0;
+        try {
+            ans = eval(nowNum);
+        }
+        catch (e) {
+            ans = "Error!!!!";
+        }
+        document.getElementsByClassName("display")[0].value = '123';
+    }
+    else if(event.key == 'Backspace') {
+        document.getElementsByClassName("display")[0].value = nowNum.slice(0, -1);
+    }
+}
